f02a7f454a02ac902b8cec37cf061df965c8b17c
junyong <rlawnsdyd530@gmail.com>
2025-07-21 22:08:47 +0900

    feat: 유저 정보 조회 테스트 API 추가

--- src/main/java/busanVibe/busan/domain/test/TestController.java
+++ src/main/java/busanVibe/busan/domain/test/TestController.java
@@ -1,15 +1,8 @@
 package busanVibe.busan.domain.test;
 
-import busanVibe.busan.domain.test.TestResponseDto.TestDto;
 import busanVibe.busan.domain.user.service.login.AuthService;
-import busanVibe.busan.global.apiPayload.exception.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
-import java.util.ArrayList;
-import java.util.Collection;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,27 +15,12 @@ public class TestController {
   private final TestService testService;
   private final AuthService authService;
 
-  @GetMapping
-  @Operation(summary = "test", description = "testtest")
-  public ApiResponse<TestResponseDto.TestDto> testPage() {
-    TestDto test = TestDto.builder()
-        .title("제목1")
-        .description("설명1")
-        .build();
-    return ApiResponse.onSuccess(test);
-  }
-
   @GetMapping("/member")
-  @Operation(summary = "유저 정보 가져오기 test")
-  public String testMember(@AuthenticationPrincipal UserDetails userDetails) {
-    Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
-    System.out.println("authorities = " + new ArrayList<>(authorities));
-    System.out.println("userDetails.getUsername() = " + userDetails.getUsername());
-    System.out.println("authService.getCurrentUserEmail() = " + authService.getCurrentUserEmail());
-    return "hello";
+  @Operation(summary = "member info test", description = "테스트용 유저 정보 조회")
+  public String testPage() {
+    String email = authService.getCurrentUserEmail();
+    return testService.userInfoTest(email);
   }
-
-
 
 }
 
--- src/main/java/busanVibe/busan/domain/test/TestService.java
+++ src/main/java/busanVibe/busan/domain/test/TestService.java
@@ -1,17 +1,23 @@
 package busanVibe.busan.domain.test;
 
-import busanVibe.busan.domain.user.service.login.AuthService;
+import busanVibe.busan.domain.user.domain.User;
+import busanVibe.busan.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class TestService {
 
-  private final AuthService authService;
+  private final UserRepository userRepository;
 
-  public String test1() {
-    return null;
+  @Transactional(readOnly = true)
+  public String userInfoTest(String email) {
+
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(() -> new RuntimeException("존재하지 않는 이메일"));
+    return "email: " + user.getEmail() + "\nname: " + user.getNickname() + "\nimg: " + user.getProfile_image();
   }
 
 }
