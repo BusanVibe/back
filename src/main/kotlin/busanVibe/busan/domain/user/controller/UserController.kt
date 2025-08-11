45ad950b91ae13bdb70532280c865d8b8a730730
junyong <rlawnsdyd530@gmail.com>
2025-08-11 13:43:35 +0900

    feat: 딥링크 적용하여 로그인 방식 변경

--- src/main/kotlin/busanVibe/busan/domain/user/controller/UserController.kt
+++ src/main/kotlin/busanVibe/busan/domain/user/controller/UserController.kt
@@ -2,37 +2,36 @@ package busanVibe.busan.domain.user.controller
 
 import busanVibe.busan.domain.user.data.dto.UserResponseDTO
 import busanVibe.busan.domain.user.service.UserCommandService
+import busanVibe.busan.domain.user.util.LoginRedirectUtil
 import busanVibe.busan.global.apiPayload.exception.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+import java.net.URLEncoder
 
 @Tag(name = "유저 관련 API", description = "로그인과 분리할수도 있습니다")
 @RestController
 @RequestMapping("/users")
 class UserController (
-    private val userCommandService: UserCommandService
+    private val userCommandService: UserCommandService,
+    private val loginRedirectUtil: LoginRedirectUtil
 ){
 
     private val log = LoggerFactory.getLogger(UserController::class.java)
 
     @GetMapping("/oauth/kakao")
-    fun callBack(@RequestParam("code") code: String): ResponseEntity<ApiResponse<UserResponseDTO.LoginDto>> {
-        log.info("login...")
-        log.info("code: $code")
-
+    fun callBack(@RequestParam("code") code: String): ResponseEntity<Void> {
         val userResponse: UserResponseDTO.LoginDto = userCommandService.loginOrRegisterByKakao(code)
-        return ResponseEntity.ok()
-            .header(
-                HttpHeaders.AUTHORIZATION,
-                "Bearer " + userResponse.tokenResponseDTO.accessToken
-            )
-            .body(ApiResponse.onSuccess(userResponse))
+        val redirectHeader = loginRedirectUtil.getRedirectHeader(userResponse)
+        return ResponseEntity.status(HttpStatus.FOUND).headers(redirectHeader).build()
 
     }
 
---
+++ src/main/kotlin/busanVibe/busan/domain/user/util/LoginRedirectUtil.kt
@@ -0,0 +1,28 @@
+package busanVibe.busan.domain.user.util
+
+import busanVibe.busan.domain.user.data.dto.UserResponseDTO
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+import java.net.URI
+import java.net.URLEncoder
+
+@Component
+class LoginRedirectUtil(
+    @Value("\${spring.kakao.deep-link}")
+    private val deepLink: String
+){
+
+    fun getRedirectHeader(userResponse: UserResponseDTO.LoginDto): HttpHeaders{
+        val accessTokenEncoded = URLEncoder.encode(userResponse.tokenResponseDTO.accessToken, "UTF-8")
+        val refreshTokenEncoded = URLEncoder.encode(userResponse.tokenResponseDTO.refreshToken, "UTF-8")
+
+        val redirectUrl = "$deepLink?accessToken=$accessTokenEncoded&refreshToken=$refreshTokenEncoded"
+
+        val headers = HttpHeaders()
+        headers.location = URI.create(redirectUrl)
+        return headers
+
+    }
+
+}
